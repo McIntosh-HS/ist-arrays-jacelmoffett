0a12d7cb91a26f355acb33d65a6e6a4d489df986
jacelmoffett <jacelmoffett@gmail.com>
2024-02-07 15:41:38 -0500

    Updated char arrays

--- chararray.c
+++ chararray.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-	char name[] = "First Last"; // edit this line
-	char user[N];               // create this line and replace N with however many elements you think is necessary
-	
+	char name[] = "Jace Moffett"; // edit this line
+	char user[69];               // create this line and replace N with however many elements you think is necessary
+
 	// Prompt the user for their name
 	// Use scanf("%s", &user) to store the user's name
+    printf("What is your name? ");
 	scanf("%s", &user);
 
-	printf("You name is %s...\n", , );
+	nameprintf("Hello %s! Nice to meet you. MY name is %s\n",user ,name );
 
 	return 0;
 }
